dab12942117b3f08e7b81c677bd632422f1b6df6
Stenford <varonapika@gmail.com>
2024-09-27 23:29:40 +0200

    try fix bugs

--- lingo-frontend/src/components/Footer.jsx
+++ lingo-frontend/src/components/Footer.jsx
@@ -5,48 +5,48 @@ const Footer = () => {
     return (
         <nav className=" w-full py-[14px] bg-[#F6F7FF] dark:bg-[#936dff] rounded-t shadow h-[80px] mt-[-14px] flex-shrink-0">
             <ul className="select-none max-w-[550px] w-full m-auto flex justify-around">
-                
+                <li>
                     <NavLink
-                        className={({ isActive }) =>
+                        className={({isActive}) =>
                             `w-full h-full flex flex-col items-center dark:text-[#F3F7FF] text-[#282950] duration-300 ${
                                 isActive ? 'opacity-1' : 'opacity-60'
                             }`
                         }
                         to="/"
-                        style={{ textDecoration: 'none' }}
+                        style={{textDecoration: 'none'}}
                     >
-                        <RiCrosshair2Line className="text-2xl mb-1" />
+                        <RiCrosshair2Line className="text-2xl mb-1"/>
                         <span>Training</span>
                     </NavLink>
-                
-                
+                </li>
+                <li>
                     <NavLink
-                        className={({ isActive }) =>
+                        className={({isActive}) =>
                             `w-full h-full flex flex-col items-center dark:text-[#F3F7FF] text-[#282950] duration-300 ${
                                 isActive ? 'opacity-1' : 'opacity-60'
                             }`
                         }
                         to="/library"
-                        style={{ textDecoration: 'none' }}
+                        style={{textDecoration: 'none'}}
                     >
-                        <RiMap2Line className="text-2xl mb-1" />
+                        <RiMap2Line className="text-2xl mb-1"/>
                         <span>Library</span>
                     </NavLink>
-                
-                
+                </li>
+                <li>
                     <NavLink
-                        className={({ isActive }) =>
+                        className={({isActive}) =>
                             `w-full h-full flex flex-col items-center dark:text-[#F3F7FF] text-[#282950] duration-300 ${
                                 isActive ? 'opacity-1' : 'opacity-60'
                             }`
                         }
                         to="/me"
-                        style={{ textDecoration: 'none' }}
+                        style={{textDecoration: 'none'}}
                     >
-                        <RiUser5Line className="text-2xl mb-1" />
+                        <RiUser5Line className="text-2xl mb-1"/>
                         <span>Profile</span>
                     </NavLink>
-                
+                </li>
             </ul>
         </nav>
     );
