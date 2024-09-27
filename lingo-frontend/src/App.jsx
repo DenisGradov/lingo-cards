8d30738049135af4d76946e34a7ebcdcad8882c0
Stenford <varonapika@gmail.com>
2024-09-27 23:45:53 +0200

    try fix bugs

--- lingo-frontend/src/App.jsx
+++ lingo-frontend/src/App.jsx
@@ -3,20 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Training from './components/Training';
 import Library from './components/Library';
 import Footer from './components/Footer';
-import useThemeStore from './store/themeStore';
-import { useEffect } from 'react';
 import UserProfile from "./components/Me.jsx";
 
 function App() {
 
-    useEffect(() => {
-        const root = window.document.documentElement;
-        if (isDark) {
-            root.classList.add('dark');
-        } else {
-            root.classList.remove('dark');
-        }
-    }, [isDark]);
+
 
     return (
         <div className="min-h-screen h-full flex flex-col bg-[#fff] dark:bg-[#282950] text-black dark:text-white">
