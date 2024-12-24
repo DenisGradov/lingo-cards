46fed01ddd96bd5b43ce0be085b6455c2ebf5eea
kettikat09 <glebstratiy@192.168.0.119>
2024-12-25 01:53:39 +0200

    fix: added ts files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,16 +11,14 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const PORT = process.env.NODE_ENV === 'test' ? 0 : process.env.BACKEND_PORT || 5001;
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : [];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['*'];
 
 if (process.env.NODE_ENV !== 'test') {
   initDatabase();
 }
 
 function handleCorsPreflight(res: ServerResponse, origin?: string): void {
-  if (process.env.NODE_ENV === 'test' || (origin && allowedOrigins.includes(origin))) {
+  if (origin && (allowedOrigins.includes('*') || allowedOrigins.includes(origin))) {
     res.setHeader('Access-Control-Allow-Origin', origin || '*');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT');
@@ -35,17 +33,17 @@ function handleCorsPreflight(res: ServerResponse, origin?: string): void {
 }
 
 function handleCorsHeaders(res: ServerResponse, origin?: string): boolean {
-  if (process.env.NODE_ENV === 'test' || (origin && allowedOrigins.includes(origin))) {
+  if (origin && (allowedOrigins.includes('*') || allowedOrigins.includes(origin))) {
     res.setHeader('Access-Control-Allow-Origin', origin || '*');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return true;
   } else {
     res.statusCode = 403;
     res.end('CORS error: Origin not allowed');
     return false;
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  return true;
 }
 
 const app = (req: IncomingMessage, res: ServerResponse): void => {
